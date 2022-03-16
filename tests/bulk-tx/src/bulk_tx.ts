62c0e001ab0f00ed7fdc9d5db54f3a81bde0d985
Kailas <kailaskr05@gmail.com>
2022-03-16 08:32:04 +0530

    upd:some changes in script and added comments

--- tests/bulk-tx/src/bulk_tx.ts
+++ tests/bulk-tx/src/bulk_tx.ts
@@ -1,52 +1,50 @@
 import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
 import { SignerResult } from '@polkadot/api/types';
 import type { EventRecord, ExtrinsicStatus, H256, SignedBlock } from '@polkadot/types/interfaces';
-import type { ISubmittableResult ,SignatureOptions } from '@polkadot/types/types';
+import type { ISubmittableResult, SignatureOptions } from '@polkadot/types/types';
 
 const ALICE = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
-const HERMEZ = '5CAezZ6kHGHY2ZUVvpqpJ9v7xSPm5Xhdj2F7H7k2iKyRzRDH';
-const herMnemonic = 'very noise grant jealous elegant update famous own pulp tongue soccer true';
-
 const keyring = new Keyring({ type: 'sr25519' });
 
+//batch size and payload size are set here
 const batch = 3;
 const size = 100;
 
-async function createApi():Promise<ApiPromise> {
+async function createApi(): Promise<ApiPromise> {
   // Initialise the provider to connect to the local node
   // const provider = new WsProvider('wss://polygon-da-explorer.matic.today/ws');
   const provider = new WsProvider('ws://127.0.0.1:9944');
 
   // Create the API and wait until ready
-  return ApiPromise.create({ 
+  return ApiPromise.create({
     provider,
     types: {
-        DataLookup: {
-          size: 'u32',
-          index: 'Vec<(u32,u32)>'
-        },
-        KateExtrinsicRoot: {
-          hash: 'Hash',
-          commitment: 'Vec<u8>',
-          rows: 'u16',
-          cols: 'u16'
-        },
-        KateHeader: {
-          parentHash: 'Hash',
-          number: 'Compact<BlockNumber>',
-          stateRoot: 'Hash',
-          extrinsicsRoot: 'KateExtrinsicRoot',
-          digest: 'Digest',
-          appDataLookup: 'DataLookup'
-        },
-        Header: 'KateHeader',
-        AppId: 'u32',
-        // CheckAppId: {
-        //     extra: {
-        //         appId: 'u32', 
-        //     },
-        //     types: {}
-        // }
+      DataLookup: {
+        size: 'u32',
+        index: 'Vec<(u32,u32)>'
+      },
+      KateExtrinsicRoot: {
+        hash: 'Hash',
+        commitment: 'Vec<u8>',
+        rows: 'u16',
+        cols: 'u16'
+      },
+      KateHeader: {
+        parentHash: 'Hash',
+        number: 'Compact<BlockNumber>',
+        stateRoot: 'Hash',
+        extrinsicsRoot: 'KateExtrinsicRoot',
+        digest: 'Digest',
+        appDataLookup: 'DataLookup'
+      },
+      Header: 'KateHeader',
+      AppId: 'u32',
+      // CheckAppId: {
+      //     extra: {
+      //         appId: 'u32', 
+      //     },
+      //     types: {}
+      // }
     },
     signedExtensions: {
       CheckAppId: {
@@ -59,16 +57,17 @@ async function createApi():Promise<ApiPromise> {
   });
 }
 
-function generateData(size:number):string {
-    let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for ( let i = 0; i < size; i++ ) {
-        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-    }
-    return result
+//generating random data as per payload specified
+function generateData(size: number): string {
+  let randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < size; i++) {
+    result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result
 }
 
-async function getNonce (api:ApiPromise, address: any) {
+async function getNonce(api: ApiPromise, address: any) {
   const nonce = (await api.rpc.system.accountNextIndex(address)).toNumber();
   return nonce;
 }
@@ -77,47 +76,47 @@ interface SignatureOptionsNew extends SignatureOptions {
   app_id: number
 }
 
-async function sendTx(api:ApiPromise, sender:any, nonce:any):Promise<any> {
-    try{
-      let data = generateData(size);
-      const nonc = await api.rpc.system.accountNextIndex(sender.address);
-      let submit = await api.tx.dataAvailability.submitData(data);
-      const options: Partial<any> = {app_id: 1, nonce: -1 }
-      const res = await submit
+async function sendTx(api: ApiPromise, sender: any, nonce: any): Promise<any> {
+  try {
+    let data = generateData(size);
+    const nonc = await api.rpc.system.accountNextIndex(sender.address);
+    let submit = await api.tx.dataAvailability.submitData(data);
+    const options: Partial<any> = { app_id: 1, nonce: -1 }
+    const res = await submit
       .signAndSend(
-          sender,  // sender
-          options, // options
-          (result: ISubmittableResult) => {
+        sender,  // sender
+        options, // options
+        (result: ISubmittableResult) => {
           //uncomment the below line👇 to see the whole status flow of the transaction
           // console.log(`Tx status: ${result.status}`);
           if (result.status.isReady) {
-              console.log(`result is ready with nonce ${nonce}`)
+            console.log(`result is ready with nonce ${nonce}`)
           }
           if (result.status.isInBlock) {
-              let block_hash = result.status.asInBlock;
-              console.log(`\nExtrinsic hash: ${result.txHash} with nonce ${nonce} is in block`);
+            let block_hash = result.status.asInBlock;
+            console.log(`\nExtrinsic hash: ${result.txHash} with nonce ${nonce} is in block`);
           }
-        
-      });
 
-        return res;
-    } catch (e) {
-        console.log(e);
-        process.exit(0);
-    }
+        });
+
+    return res;
+  } catch (e) {
+    console.log(e);
+    process.exit(0);
+  }
 }
 
-const sendTxs = async (api:any, sender:any, nonce:any) => {
+const sendTxs = async (api: any, sender: any, nonce: any) => {
 
   let non = await getNonce(api, sender.address);
   const results = [];
-  for (let i=0 ; i<batch; i++) {
+  for (let i = 0; i < batch; i++) {
     const result = await sendTx(api, sender, non)
     results.push(result);
     non = non + 1
   }
 
-  return results ;
+  return results;
 }
 
 
@@ -129,12 +128,17 @@ const sendTxs = async (api:any, sender:any, nonce:any) => {
 
 async function main() {
 
-    const api = await createApi(); 
-    const alice = keyring.addFromUri('//Alice');
-    const bob = keyring.addFromUri('//Bob');
-    const metadata = await api.rpc.state.getMetadata();
-    let nonce = await getNonce(api, ALICE);
-    let tx = await sendTxs(api, alice, nonce); 
+  const api = await createApi();
+  const alice = keyring.addFromUri('//Alice');
+  const bob = keyring.addFromUri('//Bob');
+  const metadata = await api.rpc.state.getMetadata();
+  /*@note: here ALICE test account is used.
+  You can use your own account mnemonic using the below code
+  const mnemonic = 'your mneomnic';
+  const acc = keyring.addFromUri(Mnemonic, 'sr25519'); and its address can be used by `acc.address`
+  */
+  let nonce = await getNonce(api, ALICE);
+  let tx = await sendTxs(api, alice, nonce);
 
 }
 main().catch(console.error)
