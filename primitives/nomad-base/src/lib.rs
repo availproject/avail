143237226a3b9c1c7cbdd3da85eead348ffec0ee
Luke Tchang <ltchang@stanford.edu>
2022-07-14 13:50:44 -0400

    adds nomad-base to primitives

---
+++ primitives/nomad-base/src/lib.rs
@@ -0,0 +1,93 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use frame_support::pallet_prelude::*;
+use nomad_core::{home_domain_hash, NomadState, SignedUpdate};
+use primitive_types::{H160, H256};
+
+#[cfg(feature = "testing")]
+pub mod testing;
+
+#[derive(Clone, Copy, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+pub struct NomadBase {
+	pub state: NomadState,
+	pub local_domain: u32,
+	pub updater: H160,
+}
+
+impl Default for NomadBase {
+	fn default() -> Self {
+		Self {
+			state: NomadState::Active,
+			local_domain: Default::default(),
+			updater: Default::default(),
+		}
+	}
+}
+
+pub enum NomadBaseError {
+	FailedInitialization,
+}
+
+impl NomadBase {
+	pub fn new(local_domain: u32, updater: H160) -> Self {
+		Self { local_domain, updater, state: Default::default() }
+	}
+
+	pub fn state(&self) -> NomadState {
+		self.state
+	}
+
+	pub fn local_domain(&self) -> u32 {
+		self.local_domain
+	}
+
+	pub fn updater(&self) -> H160 {
+		self.updater
+	}
+
+	pub fn home_domain_hash(&self) -> H256 {
+		home_domain_hash(self.local_domain())
+	}
+
+	pub fn set_state(&mut self, new_state: NomadState) {
+		self.state = new_state
+	}
+
+	pub fn set_updater(&mut self, new_updater: H160) {
+		self.updater = new_updater
+	}
+
+	pub fn is_updater_signature(&self, signed_update: &SignedUpdate) -> bool {
+		signed_update.verify(self.updater).is_ok()
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[cfg(feature = "testing")]
+	use crate::testing::{TEST_LOCAL_DOMAIN, TEST_NOMAD_BASE, TEST_UPDATER};
+	use ethers_signers::LocalWallet;
+	use nomad_core::test_utils::Updater;
+
+	#[test]
+	#[cfg(feature = "testing")]
+	fn it_accepts_valid_signature() {
+		let valid_signed = TEST_UPDATER.sign_update(H256::repeat_byte(1));
+		assert!(TEST_NOMAD_BASE.is_updater_signature(&valid_signed));
+	}
+
+	#[test]
+	#[cfg(feature = "testing")]
+	fn it_rejects_invalid_signature() {
+		let random_signer: LocalWallet =
+			"2222222222222222222222222222222222222222222222222222222222222222"
+				.parse()
+				.unwrap();
+		let random_updater = Updater::new(TEST_LOCAL_DOMAIN, random_signer);
+		let invalid_signed = random_updater.sign_update(H256::repeat_byte(1));
+
+		assert!(!TEST_NOMAD_BASE.is_updater_signature(&invalid_signed));
+	}
+}
---
+++ primitives/nomad-base/src/testing.rs
@@ -0,0 +1,17 @@
+use crate::NomadBase;
+use ethers_signers::LocalWallet;
+use nomad_core::test_utils::Updater;
+use once_cell::sync::Lazy;
+
+pub const TEST_LOCAL_DOMAIN: u32 = 1111;
+pub const TEST_UPDATER_PRIVKEY: &str =
+	"1111111111111111111111111111111111111111111111111111111111111111";
+
+pub static TEST_UPDATER: Lazy<Updater> = Lazy::new(|| {
+	let signer: LocalWallet = TEST_UPDATER_PRIVKEY.parse().unwrap();
+
+	Updater::new(TEST_LOCAL_DOMAIN, signer)
+});
+
+pub static TEST_NOMAD_BASE: Lazy<NomadBase> =
+	Lazy::new(|| NomadBase::new(TEST_LOCAL_DOMAIN, TEST_UPDATER.address()));
