551cdeac1d2a72d8f69b3d879c5c8fd45f110be3
Luka Borkovic <luka.borkovic@ethernal.tech>
2022-08-30 17:42:36 +0200

    Fix encoding of Avail extrinsic for subxt. Also fixes extrinsics hashing problem.

--- avail-subxt/examples/max_block_submit.rs
+++ avail-subxt/examples/max_block_submit.rs
@@ -1,9 +1,8 @@
-use avail_subxt::avail::runtime_types::frame_support::storage::bounded_vec::BoundedVec;
-use avail_subxt::*;
-use sp_keyring::AccountKeyring;
 use std::time::Instant;
-use subxt::ext::sp_core::H256;
-use subxt::{tx::PairSigner, OnlineClient};
+
+use avail_subxt::{avail::runtime_types::frame_support::storage::bounded_vec::BoundedVec, *};
+use sp_keyring::AccountKeyring;
+use subxt::{ext::sp_core::H256, tx::PairSigner, OnlineClient};
 
 /// This example attempts to submit data to fill the entire block. Note that this doesn't guarantee
 /// that the block will be filled, but if you submit more than a full block, then it will spill over
@@ -13,31 +12,31 @@ use subxt::{tx::PairSigner, OnlineClient};
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let signer = PairSigner::new(AccountKeyring::Alice.pair());
+	let signer = PairSigner::new(AccountKeyring::Alice.pair());
 
-    let api = OnlineClient::<AvailConfig>::new().await?;
-    let size: usize = 2 * 1024 * 1024;
-    let max_size: usize = 16 * 1024;
-    let num_chunks = size / max_size;
-    let mut hashes = Vec::<H256>::new();
-    let extrinsic_params = AvailExtrinsicParams::new_with_app_id(1);
-    let start = Instant::now();
-    for i in 1..=num_chunks {
-        let data_transfer =
-            avail::tx()
-                .data_availability()
-                .submit_data(BoundedVec(vec![(i & 255) as u8; max_size]));
-        let h = api
-            .tx()
-            .sign_and_submit(&data_transfer, &signer, extrinsic_params.clone())
-            .await
-            .unwrap();
-        println!("hash #{i}: {h}");
-        hashes.push(h);
-    }
-    let end = start.elapsed();
+	let api = OnlineClient::<AvailConfig>::new().await?;
+	let size: usize = 2 * 1024 * 1024;
+	let max_size: usize = 16 * 1024;
+	let num_chunks = size / max_size;
+	let mut hashes = Vec::<H256>::new();
+	let extrinsic_params = AvailExtrinsicParams::new_with_app_id(1);
+	let start = Instant::now();
+	for i in 1..=num_chunks {
+		let data_transfer =
+			avail::tx()
+				.data_availability()
+				.submit_data(BoundedVec(vec![(i & 255) as u8; max_size]));
+		let h = api
+			.tx()
+			.sign_and_submit(&data_transfer, &signer, extrinsic_params.clone())
+			.await
+			.unwrap();
+		println!("hash #{i}: {h}");
+		hashes.push(h);
+	}
+	let end = start.elapsed();
 
-    println!("Done in {end:?}!");
+	println!("Done in {end:?}!");
 
-    Ok(())
+	Ok(())
 }
