d8381fd29df5fb96f9d64d4d113b6b6bc6ecf253
Luka Borkovic <luka.borkovic@ethernal.tech>
2022-08-30 17:42:36 +0200

    Fix encoding of Avail extrinsic for subxt. Also fixes extrinsics hashing problem.

--- avail-subxt/examples/accounts_from_mnemonics.rs
+++ avail-subxt/examples/accounts_from_mnemonics.rs
@@ -1,46 +1,48 @@
 use avail_subxt::*;
 use sp_keyring::sr25519::sr25519;
-use subxt::ext::sp_core::Pair;
-use subxt::ext::sp_runtime::MultiAddress;
-use subxt::{tx::PairSigner, OnlineClient};
+use subxt::{
+	ext::{sp_core::Pair, sp_runtime::MultiAddress},
+	tx::PairSigner,
+	OnlineClient,
+};
 
 /// This example demonstrates using mnemonic seed for generating signer pairs. It creates Alice and Bob
 /// from seeds, but could also be used for an arbitrary account.
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let signer_a: PairSigner<AvailConfig, sr25519::Pair> = PairSigner::new(
-        Pair::from_string_with_seed(
-            "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice",
-            None,
-        )
-        .unwrap()
-        .0,
-    );
-    let signer_b: PairSigner<AvailConfig, sr25519::Pair> = PairSigner::new(
-        Pair::from_string_with_seed(
-            "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Bob",
-            None,
-        )
-        .unwrap()
-        .0,
-    );
-    let api = OnlineClient::<AvailConfig>::new().await?;
-    let balance_transfer = avail::tx()
-        .balances()
-        .transfer(MultiAddress::Id(signer_b.account_id().clone()), 2);
-    let _ = api
-        .tx()
-        .sign_and_submit_then_watch(
-            &balance_transfer,
-            &signer_a,
-            AvailExtrinsicParams::new_with_app_id(0),
-        )
-        .await
-        .unwrap()
-        .wait_for_in_block()
-        .await
-        .unwrap();
+	let signer_a: PairSigner<AvailConfig, sr25519::Pair> = PairSigner::new(
+		Pair::from_string_with_seed(
+			"bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice",
+			None,
+		)
+		.unwrap()
+		.0,
+	);
+	let signer_b: PairSigner<AvailConfig, sr25519::Pair> = PairSigner::new(
+		Pair::from_string_with_seed(
+			"bottom drive obey lake curtain smoke basket hold race lonely fit walk//Bob",
+			None,
+		)
+		.unwrap()
+		.0,
+	);
+	let api = OnlineClient::<AvailConfig>::new().await?;
+	let balance_transfer = avail::tx()
+		.balances()
+		.transfer(MultiAddress::Id(signer_b.account_id().clone()), 2);
+	let _ = api
+		.tx()
+		.sign_and_submit_then_watch(
+			&balance_transfer,
+			&signer_a,
+			AvailExtrinsicParams::new_with_app_id(0),
+		)
+		.await
+		.unwrap()
+		.wait_for_in_block()
+		.await
+		.unwrap();
 
-    Ok(())
+	Ok(())
 }
--- avail-subxt/examples/headers.rs
+++ avail-subxt/examples/headers.rs
@@ -8,47 +8,46 @@ use subxt::{rpc::BlockNumber, OnlineClient};
 const BATCH_NUM: usize = 1000;
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let api = OnlineClient::<AvailConfig>::new().await?;
+	let api = OnlineClient::<AvailConfig>::new().await?;
 
-    let head_block = api.rpc().block(None).await.unwrap().unwrap();
+	let head_block = api.rpc().block(None).await.unwrap().unwrap();
 
-    let block_num = head_block.block.header.number;
-    println!("Current head: {block_num}");
-    println!(
-        "Current head block extrinsic: {block:?}",
-        block = head_block.block.extrinsics
-    );
-    println!(
-        "Current head block header: {header:?}",
-        header = head_block.block.header
-    );
+	let block_num = head_block.block.header.number;
+	println!("Current head: {block_num}");
+	println!(
+		"Current head block extrinsic: {block:?}",
+		block = head_block.block.extrinsics
+	);
+	println!(
+		"Current head block header: {header:?}",
+		header = head_block.block.header
+	);
 
-    let mut headers = vec![];
+	let mut headers = vec![];
 
-    for batch in (1u32..=block_num)
-        .collect::<Vec<_>>()
-        .chunks(BATCH_NUM)
-        .map(|e| {
-            join_all(
-                e.iter()
-                    .map(|n| {
-                        api.rpc()
-                            .block_hash(Some(BlockNumber::from(*n)))
-                            .and_then(|h| api.rpc().header(h))
-                    })
-                    .collect::<Vec<_>>(),
-            )
-        })
-    {
-        headers.extend(batch.await);
-    }
-    println!("Headers: {num}", num = headers.len());
+	for batch in (1u32..=block_num)
+		.collect::<Vec<_>>()
+		.chunks(BATCH_NUM)
+		.map(|e| {
+			join_all(
+				e.iter()
+					.map(|n| {
+						api.rpc()
+							.block_hash(Some(BlockNumber::from(*n)))
+							.and_then(|h| api.rpc().header(h))
+					})
+					.collect::<Vec<_>>(),
+			)
+		}) {
+		headers.extend(batch.await);
+	}
+	println!("Headers: {num}", num = headers.len());
 
-    assert_eq!(
-        headers.len(),
-        block_num as usize,
-        "Didn't get the same number of block headers."
-    );
+	assert_eq!(
+		headers.len(),
+		block_num as usize,
+		"Didn't get the same number of block headers."
+	);
 
-    Ok(())
+	Ok(())
 }
