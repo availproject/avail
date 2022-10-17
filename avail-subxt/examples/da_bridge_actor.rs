922b55179b91454366b1c0cf7a0f33b84fd69f4f
Luke Tchang <ltchang@stanford.edu>
2022-10-17 15:46:39 -0700

    da-bridge actor takes in env vars

--- avail-subxt/examples/da_bridge_actor.rs
+++ avail-subxt/examples/da_bridge_actor.rs
@@ -1,35 +1,58 @@
 use avail_subxt::*;
-use sp_keyring::AccountKeyring;
-use subxt::{ext::sp_core::H160, tx::PairSigner, OnlineClient};
-
-const DESTINATION_DOMAIN: u32 = 1000;
-const DA_BRIDGE_ROUTER_ADDRESS: &str = "0x3f28a3e66326c3aa494d4f8e9477d1397ee94432";
+use subxt::{
+	ext::sp_core::{ecdsa::Pair, Pair as PairT, H160},
+	tx::PairSigner,
+	OnlineClient,
+};
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let api = OnlineClient::<AvailConfig>::from_url("ws://127.0.0.1:9944").await?;
-	let signer = PairSigner::new(AccountKeyring::Alice.pair());
+	let rpc = std::env::var("RPC_URL").expect("missing RPC_URL");
+	let priv_key = std::env::var("PRIVATE_KEY").expect("missing PRIVATE_KEY");
+	let interval: u32 = std::env::var("INTERVAL_BLOCKS")
+		.unwrap_or("10".into())
+		.parse()
+		.unwrap();
+	let destination_domain = std::env::var("DESTINATION_DOMAIN")
+		.expect("missing DESTINATION_DOMAIN")
+		.parse()
+		.unwrap();
+	let da_bridge_router_addr =
+		std::env::var("DA_BRIDGE_ROUTER_ADDRESS").expect("missing DA_BRIDGE_ROUTER_ADDRESS");
+
+	let api = OnlineClient::<AvailConfig>::from_url(rpc).await?;
+
+	let pair = Pair::from_string(&priv_key, None).unwrap();
+	let signer = PairSigner::new(pair);
+	println!("Tx signer AccountId: {}", signer.account_id());
 
 	let mut finalized_blocks = api.rpc().subscribe_finalized_blocks().await?;
 
 	while let Some(finalized_block) = finalized_blocks.next().await {
 		let header = finalized_block.unwrap();
+
+		// Want to submit every interval blocks
+		let number: u32 = header.number.into();
+		if number % interval != 0 {
+			continue;
+		}
+
 		println!("Finalized block header: {:?}", &header);
 		println!("Header data root: {:?}", header.extrinsics_root.data_root);
 
 		if let Some(block_hash) = api.rpc().block_hash(Some(header.number.into())).await? {
 			println!("Block hash: {:?}", block_hash);
 
-			let bridge_router_eth_addr: H160 = DA_BRIDGE_ROUTER_ADDRESS.parse().unwrap();
+			let bridge_router_eth_addr: H160 = da_bridge_router_addr.parse().unwrap();
 			let tx = avail::tx().da_bridge().try_dispatch_data_root(
-				DESTINATION_DOMAIN,
+				destination_domain,
 				bridge_router_eth_addr.into(),
-				header.into(),			
+				header.into(),
 			);
 
 			println!(
 				"Sending finalized block header. Domain: {}. Recipient: {}",
-				DESTINATION_DOMAIN, DA_BRIDGE_ROUTER_ADDRESS
+				destination_domain, da_bridge_router_addr
 			);
 			let h = api
 				.tx()
