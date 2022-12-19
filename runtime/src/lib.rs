b5a52ea410fb1935af34c533e4fccd742ad400ad
miguelDD1 <miguel@polygon.technology>
2022-12-19 13:53:32 +0100

    Enable & update benchmarks

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -792,15 +792,11 @@ parameter_types! {
 use sp_runtime::traits::Convert;
 pub struct BalanceToU256;
 impl Convert<Balance, sp_core::U256> for BalanceToU256 {
-	fn convert(balance: Balance) -> sp_core::U256 {
-		sp_core::U256::from(balance)
-	}
+	fn convert(balance: Balance) -> sp_core::U256 { sp_core::U256::from(balance) }
 }
 pub struct U256ToBalance;
 impl Convert<sp_core::U256, Balance> for U256ToBalance {
-	fn convert(n: sp_core::U256) -> Balance {
-		n.try_into().unwrap_or(Balance::max_value())
-	}
+	fn convert(n: sp_core::U256) -> Balance { n.try_into().unwrap_or(Balance::max_value()) }
 }
 
 impl pallet_nomination_pools::Config for Runtime {
