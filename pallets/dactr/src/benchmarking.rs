eddbe926dcf2df3bdee9313f1b8bb0b9dbcf6d97
root <root@LAPTOP-UD2UURQF>
2023-06-16 16:10:07 +0200

    [wip] continue benchmarking tests on kate

--- pallets/dactr/src/benchmarking.rs
+++ pallets/dactr/src/benchmarking.rs
@@ -120,62 +120,62 @@ benchmarks! {
 			  T: Send + Sync + Debug + StaticTypeInfo
 	}
 
-	// create_application_key {
-	// 	let max_key_len = T::MaxAppKeyLength::get();
+	create_application_key {
+		let max_key_len = T::MaxAppKeyLength::get();
 
-	// 	let key = generate_bounded::<AppKeyFor<T>>(max_key_len);
-	// 	let key_verify = key.clone();
+		let key = generate_bounded::<AppKeyFor<T>>(max_key_len);
+		let key_verify = key.clone();
 
-	// 	let caller = whitelisted_caller::<T::AccountId>();
-	// 	let origin = RawOrigin::Signed(caller.clone());
+		let caller = whitelisted_caller::<T::AccountId>();
+		let origin = RawOrigin::Signed(caller.clone());
 
-	// }: create_application_key(origin, key)
-	// verify {
-	// 	let info = Pallet::<T>::application_key(&key_verify);
-	// 	assert_eq!( info, Some(AppKeyInfoFor::<T> { owner: caller, id: 3.into()}));
-	// }
+	}: create_application_key(origin, key)
+	verify {
+		let info = Pallet::<T>::application_key(&key_verify);
+		assert_eq!( info, Some(AppKeyInfoFor::<T> { owner: caller, id: 3.into()}));
+	}
 
-	// submit_block_length_proposal {
-	// 	let rows = T::MaxBlockRows::get().0;
-	// 	let cols = T::MaxBlockCols::get().0;
-	// 	let origin = RawOrigin::Root;
-	// }: submit_block_length_proposal(origin, rows, cols)
+	submit_block_length_proposal {
+		let rows = T::MaxBlockRows::get().0;
+		let cols = T::MaxBlockCols::get().0;
+		let origin = RawOrigin::Root;
+	}: submit_block_length_proposal(origin, rows, cols)
 
-	// submit_data {
-	// 	let i in 1..T::MaxAppDataLength::get();
+	submit_data {
+		let i in 1..T::MaxAppDataLength::get();
 
-	// 	let data = generate_bounded::<AppDataFor<T>>(i);
-	// 	let origin = RawOrigin::Signed(whitelisted_caller::<T::AccountId>());
+		let data = generate_bounded::<AppDataFor<T>>(i);
+		let origin = RawOrigin::Signed(whitelisted_caller::<T::AccountId>());
 
-	// }: submit_data(origin, data)
+	}: submit_data(origin, data)
 
-	// data_root {
-	// 	let i in 0..T::MaxAppDataLength::get();
+	data_root {
+		let i in 0..T::MaxAppDataLength::get();
 
-	// 	let data = generate_bounded::<AppDataFor<T>>(i);
-	// 	let opaque = submit_data_ext::<T>(data);
+		let data = generate_bounded::<AppDataFor<T>>(i);
+		let opaque = submit_data_ext::<T>(data);
 
-	// }:{
-	// 	let _data_root =submitted_data::extrinsics_root::<T::SubmittedDataExtractor, _>(once(&opaque));
-	// }
+	}:{
+		let _data_root =submitted_data::extrinsics_root::<T::SubmittedDataExtractor, _>(once(&opaque));
+	}
 
 	commitment_builder{
 		let seed = [0u8;32];
-		let i:u32 = 512;// in 32..128;//T::MaxBlockRows::get().0;
-		let j:u32 = 256;// in 32..128;//T::MaxBlockCols::get().0;
-		// let k in 0..100;//i*j*(32-2);
+		let i in 128..T::MaxBlockRows::get().0;
+		let j in 128..T::MaxBlockCols::get().0;
 		let k in 0..T::MaxAppDataLength::get();
-		let l in 0..128;
+		// let k: u32 = T::MaxAppDataLength::get();// in 0..T::MaxAppDataLength::get();
+		// let l in 0..128;
+
+		let l:u32 = 128;
 
 		info!("i: {} - j: {}", i, j);
 		info!("k: {} - l: {}", k, l);
 
 		let row = BlockLengthRows(i);
 		let col = BlockLengthColumns(j);
-		// let data = vec![0u8; k as usize];
 		let data:Vec<u8> = generate_bounded::<AppDataFor<T>>(k).to_vec();
 		let txs = vec![AppExtrinsic::from(data.to_vec()); l.try_into().unwrap()];
-		// let tx = AppExtrinsic::from(data.to_vec());
 		info!("Launching extrinsic");
 	}: {
 		#[cfg(feature = "std")]
