f3c821975c4c665074df4cc9deb8a3c32dc7ea1b
kaustubhkapatral <kaustubh.kapatral@gmail.com>
2023-09-01 15:14:36 +0530

    Added devnet workflow

---
+++ scripts/purge.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Finding no of active avail services already present on server to cover edge case of env vars being modified between workflow runs. 
+cd /etc/systemd/system
+TOTAL_COUNT=$(ls -dq *avail* | wc -l)
+
+for (( i=1; i<=$TOTAL_COUNT; i++ ))
+do 
+    sudo systemctl stop avail-val-${i}.service
+    sudo systemctl stop avail-full-${i}.service
+    sudo systemctl stop avail-light-${i}.service
+    sudo systemctl disable avail-light-${i}.service
+    sudo systemctl disable avail-full-${i}.service
+    sudo systemctl disable avail-val-${i}.service
+    sudo rm /etc/systemd/system/avail-val-${i}.service
+    sudo rm /etc/systemd/system/avail-full-${i}.service
+    sudo rm /etc/systemd/system/avail-light-${i}.service
+done
+
+rm -rf $HOME/avail-home
+
+rm -rf $HOME/avail-keys
+
+rm -rf $HOME/avail-apps
+
+rm -rf $HOME/build
+
+rm -rf $HOME/data-avail
+
+rm -rf $HOME/light-bootstrap
+
+rm $HOME/endpoints.txt
+
+sudo rm -rf /var/www/html/*
+
+sudo systemctl daemon-reload
