e960f7f6d0bbd064b62f6245aae540fc55bb6513
Leouarz <ghalielouarzazi@hotmail.com>
2023-09-25 11:28:10 +0200

    wip: fix all features

--- core/src/header/extension/v_test.rs
+++ core/src/header/extension/v_test.rs
@@ -5,10 +5,11 @@ use serde::{Deserialize, Serialize};
 use sp_core::{RuntimeDebug, H256};
 use sp_std::vec::Vec;
 
-use crate::{asdr::DataLookup, header::extension::v1, KateCommitment};
+use crate::{header::extension::v1, v1::KateCommitment, DataLookup};
 
 #[derive(PartialEq, Eq, Clone, RuntimeDebug, TypeInfo, Encode, Decode, Default)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
 pub struct HeaderExtension {
 	pub new_field: Vec<u8>,
 	pub commitment: KateCommitment,
@@ -18,6 +19,18 @@ pub struct HeaderExtension {
 impl HeaderExtension {
 	pub fn data_root(&self) -> H256 {
 		self.commitment.data_root
+	}
+
+	pub fn app_lookup(&self) -> &DataLookup {
+		&self.app_lookup
+	}
+
+	pub fn rows(&self) -> u16 {
+		self.commitment.rows
+	}
+
+	pub fn cols(&self) -> u16 {
+		self.commitment.cols
 	}
 }
 
