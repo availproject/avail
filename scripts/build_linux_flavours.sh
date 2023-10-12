f0777712d406565777f1d583ed48538949723086
Marko Petrlic <petrlic.marko@gmail.com>
2023-10-12 13:57:23 +0200

    Simplified build linux flavour script

--- scripts/build_linux_flavours.sh
+++ scripts/build_linux_flavours.sh
@@ -1,45 +1,21 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-REPO_DIR=$SCRIPT_DIR/..
-
 FLAVOUR="${FLAVOUR:-ubuntu-2204}"
 SYSTEM="${SYSTEM:-docker}"
 
-if [ -z "$FLAVOUR" ]; then
-    select FLAVOUR in ubuntu-2204 ubuntu-2304 fedora-37 fedora-38 debian-11 debian-12 arch
-    do
-        break;
-    done
-fi
+IMAGE="${FLAVOUR}.Dockerfile"
+DOCKER_FILE="./scripts/linux_flavours/$IMAGE"
 
-# Get Image name
-if [ "$FLAVOUR" = "ubuntu-2204" ]; then
-    imageName="ubuntu-2204.Dockerfile"
-    elif [ "$FLAVOUR" = "ubuntu-2304" ]; then
-    imageName="ubuntu-2304.Dockerfile"
-    elif [ "$FLAVOUR" = "fedora-37" ]; then
-    imageName="fedora-37.Dockerfile"
-    elif [ "$FLAVOUR" = "fedora-38" ]; then
-    imageName="fedora-38.Dockerfile"
-    elif [ "$FLAVOUR" = "debian-11" ]; then
-    imageName="debian-11.Dockerfile"
-    elif [ "$FLAVOUR" = "debian-12" ]; then
-    imageName="debian-12.Dockerfile"
-    elif [ "$FLAVOUR" = "arch" ]; then
-    imageName="arch.Dockerfile"
-else
-    echo "Unknown option: $FLAVOUR. Use one of these: ubuntu-2204 ubuntu-2304 fedora-37 fedora-38 debian-11 debian-12 arc"
+if ! test -f "$DOCKER_FILE"; then
+    echo "Unknown option: $FLAVOUR. Use one of these: ubuntu-2204 ubuntu-2304 fedora-37 fedora-38 debian-11 debian-12 arch"
     exit 0
 fi
 
 echo "Selected flavour: $FLAVOUR"
 echo "Selected system: $SYSTEM"
-
-# Move the right directory
-cd $REPO_DIR
+echo "Selected docker file: $DOCKER_FILE"
 
 # Build the image
-"$SYSTEM" build -t availnode -f ./scripts/linux_flavours/$imageName .
+"$SYSTEM" build -t availnode -f $DOCKER_FILE .
 
 # Run the image
 mkdir -p output/$FLAVOUR
