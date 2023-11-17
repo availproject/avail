7c8659dcf14dceea8511405be8afbd87214dbe0f
Sasa Prsic <sasa@ethernal.tech>
2023-11-17 10:01:09 +0100

    Add test for setting updater.

--- pallets/succinct/src/tests.rs
+++ pallets/succinct/src/tests.rs
@@ -1,17 +1,35 @@
+use frame_support::dispatch::RawOrigin;
+use frame_support::{assert_err, assert_ok};
 use hex_literal::hex;
 use sp_runtime::testing::H256;
-use crate::Call::set_updater;
-use crate::mock::new_test_ext;
-use crate::mock::Test;
+use sp_runtime::DispatchError::BadOrigin;
+
+use crate::mock::{new_test_ext, Bridge, RuntimeOrigin, Test};
+use crate::StateStorage;
 
 #[test]
 fn test_set_updater() {
-    new_test_ext().execute_with(|| {
+	new_test_ext().execute_with(|| {
+		// Goal: Set updater - bad origin.
+		let new_updater = H256(hex!(
+			"d54593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"
+		));
+		let old_updater = H256(hex!(
+			"d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"
+		));
 
-        // Goal: Set updater.
+		let before = StateStorage::<Test>::get();
+		assert_eq!(before.updater, old_updater);
+		let bad_origin = RuntimeOrigin::from(RawOrigin::None);
+		let bad_result = Bridge::set_updater(bad_origin, new_updater);
+		assert_err!(bad_result, BadOrigin);
+		assert_eq!(before.updater, old_updater);
 
-        let new_updater = H256(hex!("d54593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d"));
-
-        set_updater::<Test> { updater: new_updater }
-    });
+		// Goal: Set updater - success.
+		let root_origin = RuntimeOrigin::from(RawOrigin::Root);
+		let success = Bridge::set_updater(root_origin, new_updater);
+		assert_ok!(success);
+		let after = StateStorage::<Test>::get();
+		assert_eq!(after.updater, new_updater);
+	});
 }
