6d8aff280120248d88081f2db265994d0fd91951
Marko Petrlic <petrlic.marko@gmail.com>
2023-11-27 16:19:59 +0100

    hope that the message now is correct. I hope

--- node/src/command.rs
+++ node/src/command.rs
@@ -61,7 +61,7 @@ impl SubstrateCli for Cli {
 
 	fn load_spec(&self, id: &str) -> std::result::Result<Box<dyn sc_service::ChainSpec>, String> {
 		let spec = match id {
-			"" => return Err("Please specify which chain you want to run, e.g. --goldberg".into()),
+			"" => return Err("Please specify which chain you want to run, e.g. --chain goldberg".into()),
 			"dev" => Box::new(chains::dev::chain_spec()),
 			"dev.tri" => Box::new(chains::dev_tri::chain_spec()),
 			"goldberg" => Box::new(chains::goldberg::chain_spec()?),
