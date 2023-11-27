de99ad1c0d1bd659ceaec9387edf32cfd9734a6c
Marko Petrlic <petrlic.marko@gmail.com>
2023-11-27 10:01:15 +0100

    Updated deno benchmarks

--- examples/deno/benchmarks/query_data_proof.ts
+++ examples/deno/benchmarks/query_data_proof.ts
@@ -1,47 +1,34 @@
 import { ApiPromise, Keyring, WsProvider } from 'https://deno.land/x/polkadot@0.2.42/api/mod.ts';
 import { API_RPC, API_TYPES, API_EXTENSIONS } from './../api_options.ts'
-import { waitForBlockInclusion, waitForBlockFinalization } from './misc.ts';
+import { prepareData } from './misc.ts';
+import { BlockFinalizationStage, BlockInclusionStage, PerformanceMeasureStage, DataSubmissionStage, DoneStage, Task } from './task.ts';
 
 const api = await ApiPromise.create({ provider: new WsProvider("ws://127.0.0.1:9944"), rpc: API_RPC, types: API_TYPES, signedExtensions: API_EXTENSIONS  });
 const alice = new Keyring({type: 'sr25519'}).addFromUri("//Alice");
 
-
 console.log("Preparing data...")
 const txCount = 100;
-const data: string[] = [];
+const data = prepareData(txCount);
 
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-const charactersLength = characters.length;
-for (let i = 0; i < txCount; ++i) {
-    let array = '';
-    let counter = 0;
-    while (counter < 16 * 1024) {
-        array += characters.charAt(Math.floor(Math.random() * charactersLength));
-        counter += 1;
-    }
-    data.push(array);
+const targetBlockNumber = (await api.rpc.chain.getHeader()).number.toNumber() + 1;
+const tasks: Task[] = [];
+const jobs = [];
+const jobCount = 10;
+
+for(let i = 0; i < jobCount; ++i) {
+    const task = new Task(`${i}`, api, data, txCount);
+
+    const customStage = new PerformanceMeasureStage(async (task) => await task.api.rpc.kate.queryDataProof(1, task.finalizedBlockHash), "Querying Data Proof");
+    const stages = [new BlockInclusionStage(targetBlockNumber + i), new DataSubmissionStage(alice), new BlockFinalizationStage(targetBlockNumber + 1 + i), customStage, new DoneStage()];
+    jobs.push(task.run(stages));
+    tasks.push(task);
 }
 
-console.log("Waiting for a new block to appear (this might take a while)...")
-const currentBlockNumber = (await api.rpc.chain.getHeader()).number.toNumber();
-await waitForBlockInclusion(api, currentBlockNumber + 1);
+await Promise.all(jobs);
 
-console.log("Submitting Data...")
-let nonce = (await api.rpc.system.accountNextIndex(alice.address)).toNumber();
-const txs = [];
-for (let i = 0; i < txCount; ++i) {
-    txs.push(api.tx.dataAvailability.submitData(data[i].toString()).signAndSend(alice, {nonce: nonce}));
-    nonce += 1;
-}
-await Promise.all(txs);
-
-console.log("Waiting for txs to be finalized (this might take a while)...")
-const targetBlockHash = await waitForBlockFinalization(api, currentBlockNumber + 2);
-
-console.log("Querying Data Proof...")
-performance.mark("start");
-const _ = await api.rpc.kate.queryDataProof(1, targetBlockHash);
-performance.mark("end");
-console.log(performance.measure('Querying Data Proof', 'start', 'end'))
+const durations = tasks.map((t) => t.measure?.duration);
+const totalTime = durations.reduce((pv, cv) => pv += cv);
+console.log(durations);
+console.log(`Total time: ${totalTime}; Average time: ${totalTime / jobCount}`);
 
 Deno.exit(0);
--- examples/deno/benchmarks/query_proof.ts
+++ examples/deno/benchmarks/query_proof.ts
@@ -1,64 +1,53 @@
 import { ApiPromise, Keyring, WsProvider } from 'https://deno.land/x/polkadot@0.2.42/api/mod.ts';
 import { API_RPC, API_TYPES, API_EXTENSIONS } from './../api_options.ts'
-import { waitForBlockInclusion, waitForBlockFinalization } from './misc.ts';
+import { prepareData } from './misc.ts';
+import { BlockFinalizationStage, BlockInclusionStage, PerformanceMeasureStage, DataSubmissionStage, DoneStage, Task } from './task.ts';
 
 const api = await ApiPromise.create({ provider: new WsProvider("ws://127.0.0.1:9944"), rpc: API_RPC, types: API_TYPES, signedExtensions: API_EXTENSIONS  });
 const alice = new Keyring({type: 'sr25519'}).addFromUri("//Alice");
 
-
 console.log("Preparing data...")
 const txCount = 100;
-const data: string[] = [];
-
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-const charactersLength = characters.length;
-for (let i = 0; i < txCount; ++i) {
-    let array = '';
-    let counter = 0;
-    while (counter < 16 * 1024) {
-        array += characters.charAt(Math.floor(Math.random() * charactersLength));
-        counter += 1;
-    }
-    data.push(array);
-}
-
-console.log("Waiting for a new block to appear (this might take a while)...")
-const currentBlockNumber = (await api.rpc.chain.getHeader()).number.toNumber();
-await waitForBlockInclusion(api, currentBlockNumber + 1);
-
-console.log("Submitting Data...")
-let nonce = (await api.rpc.system.accountNextIndex(alice.address)).toNumber();
-const txs = [];
-for (let i = 0; i < txCount; ++i) {
-    txs.push(api.tx.dataAvailability.submitData(data[i].toString()).signAndSend(alice, {nonce: nonce}));
-    nonce += 1;
-}
-await Promise.all(txs);
-
-console.log("Waiting for txs to be finalized (this might take a while)...")
-const targetBlockHash = await waitForBlockFinalization(api, currentBlockNumber + 2);
+const data = prepareData(txCount);
 
 console.log("Defining cells...")
 const count = 8500;
-const cells = [];
+const cells: [number, number][] = [];
 for (let i = 0; i < 256; ++i) {
     for (let j = 0; j < 256; ++j) {
         cells.push([i, j]);
     }
 }
+const targetBlockNumber = (await api.rpc.chain.getHeader()).number.toNumber() + 1;
+const tasks: Task[] = [];
+const jobs = [];
+const jobCount = 10;
 
-let end = 0;
-const promises = [];
-console.log("Querying 8.5k cells...")
-performance.mark("start");
-for (let counter = 0; counter < count; counter += 30) {
-    end = counter + 30;
-    end = end > count ? count : end;
-    promises.push(api.rpc.kate.queryProof(cells.slice(counter, end), targetBlockHash))
+for(let i = 0; i < jobCount; ++i) {
+    const task = new Task(`${i}`, api, data, txCount);
+
+    const customStage = new PerformanceMeasureStage(async (task) => {
+        const promises = [];
+        let end = 0;
+        for (let counter = 0; counter < count; counter += 30) {
+            end = counter + 30;
+            end = end > count ? count : end;
+            promises.push(task.api.rpc.kate.queryProof(cells.slice(counter, end), task.finalizedBlockHash))
+        }
+        
+        await Promise.all(promises);
+    }, "Querying 8.5k Cells");
+
+    const stages = [new BlockInclusionStage(targetBlockNumber + i), new DataSubmissionStage(alice), new BlockFinalizationStage(targetBlockNumber + 1 + i), customStage, new DoneStage()];
+    jobs.push(task.run(stages));
+    tasks.push(task);
 }
 
-await Promise.all(promises);
-performance.mark("end");
-console.log(performance.measure('8.5k cell retrieval', 'start', 'end'))
+await Promise.all(jobs);
+
+const durations = tasks.map((t) => t.measure?.duration);
+const totalTime = durations.reduce((pv, cv) => pv += cv);
+console.log(durations);
+console.log(`Total time: ${totalTime}; Average time: ${totalTime / jobCount}`);
 
 Deno.exit(0);
