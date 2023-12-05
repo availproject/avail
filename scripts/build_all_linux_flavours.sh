0e24155b94eee37d9985d7c122df77f00841a522
Marko Petrlic <petrlic.marko@gmail.com>
2023-12-05 20:21:11 +0100

    Added zip folder when building linux flavours

--- scripts/build_all_linux_flavours.sh
+++ scripts/build_all_linux_flavours.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-export SYSTEM="${SYSTEM:-docker}"
+export ENGINE="${ENGINE:-docker}"
+
+mkdir -p ./output/zips/
 
 for flavor in ubuntu-2004 ubuntu-2204 ubuntu-2304 ubuntu-2310 fedora-38 fedora-39 debian-11 debian-12 arch; do
     FLAVOUR=${flavor} ./scripts/build_linux_flavours.sh
-    cd ./output/"$flavor"/
-    tar czf ./../x86_64-"$flavor"-data-avail.tar.gz data-avail
-    cd ./../..
+    tar -czf ./output/zips/x86_64-"$flavor"-data-avail.tar.gz ./output/"$flavor"/data-avail
 done
