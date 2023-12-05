cb25d80a8456313799a5deea15fb7379e6bf35cc
Marko Petrlic <petrlic.marko@gmail.com>
2023-12-05 20:04:55 +0100

    Updated dockerfiles and dockerignores

--- scripts/build_all_linux_flavours.sh
+++ scripts/build_all_linux_flavours.sh
@@ -2,6 +2,7 @@
 export SYSTEM="${SYSTEM:-docker}"
 
 for flavor in ubuntu-2004 ubuntu-2204 ubuntu-2304 ubuntu-2310 fedora-38 fedora-39 debian-11 debian-12 arch; do
+    FLAVOUR=${flavor} ./scripts/build_linux_flavours.sh
     cd ./output/"$flavor"/
     tar czf ./../x86_64-"$flavor"-data-avail.tar.gz data-avail
     cd ./../..
