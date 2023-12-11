125f4666612d1abcdbd3103d3e1a523f30f8b2e2
Sasa Prsic <sasa@ethernal.tech>
2023-12-11 14:41:07 +0100

    Add test with multiple calls.

--- runtime/src/data_root_tests.rs
+++ runtime/src/data_root_tests.rs
@@ -19,6 +19,10 @@ fn submit_call() -> Vec<u8> {
 	hex!("ed018400d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d01be06880f2f6203365b508b4226fd697d3d79d3a50a5617aad714466d40ef47067225e823135b32121aa0f6f56e696f5f71107a6d44768c2fefe38cb209f7f28224000000041d014054657374207375626d69742064617461").to_vec()
 }
 
+fn submit_bridge_call() -> Vec<u8> {
+	hex!("e5018400fe65717dad0447d715f660a0a58411de509b42e6efb8375f562f58a554d5860e012441f8b68ac92828c3e66a20403e4edeb0da935f2e86ab5f58ea357a59aab3455dc4a8bc3290ca0c49a839fb98f3e40ebdfa87e7205ae7cfe943f752213c678144010400002707385465737420627269646765207478").to_vec()
+}
+
 fn submit_call_expected() -> H256 {
 	// hex!("ddf368647a902a6f6ab9f53b32245be28edc99e92f43f0004bbc2cb359814b2a").into()
 	// hex!("9c6cf805b377632c6a224e1ca035f8f6975932529a5e492e73742e4f861ba89d").into()
@@ -30,6 +34,33 @@ fn submit_call_expected() -> H256 {
 	// keccak_256(blob_root, bridge_root)
 	concat.extend_from_slice(blob_root.as_slice());
 	concat.extend_from_slice(H256::zero().as_bytes());
+	H256(keccak_256(concat.as_slice()))
+}
+
+fn submit_bridge_call_expected() -> H256 {
+	// 5736a1657b94e2f57cfa10be6296739e7158b6cc80c56fddaaa3317263d5c41d
+	// leaf is keccak256(data) -> root
+	let leaf_data = hex!("5736a1657b94e2f57cfa10be6296739e7158b6cc80c56fddaaa3317263d5c41d");
+	let bridge_root = keccak_256(leaf_data.as_slice());
+
+	let mut concat = vec![];
+	// keccak_256(blob_root, bridge_root)
+	concat.extend_from_slice(H256::zero().as_bytes());
+	concat.extend_from_slice(bridge_root.as_slice());
+	H256(keccak_256(concat.as_slice()))
+}
+
+fn submit_bridge_and_data_call_expected() -> H256 {
+	let blob_leaf_data = hex!("db45128913020d152dbee4d00a1dffebdb703425c44adbd7d7dfc7ae93d836bc");
+	let blob_root = keccak_256(blob_leaf_data.as_slice());
+
+	let bridge_leaf_data = hex!("5736a1657b94e2f57cfa10be6296739e7158b6cc80c56fddaaa3317263d5c41d");
+	let bridge_root = keccak_256(bridge_leaf_data.as_slice());
+
+	let mut concat = vec![];
+	// keccak_256(blob_root, bridge_root)
+	concat.extend_from_slice(blob_root.as_slice());
+	concat.extend_from_slice(bridge_root.as_slice());
 	H256(keccak_256(concat.as_slice()))
 }
 
@@ -74,8 +105,15 @@ fn decode_submit_call() {
 	assert_eq!(call, expected_call);
 }
 
-#[test_case( submit_call() => submit_call_expected(); "Submit data 0")]
-fn data_root_filter(extrinsic: Vec<u8>) -> H256 {
-	let opaque = OpaqueExtrinsic::decode(&mut extrinsic.as_slice()).unwrap();
-	extrinsics_root::<Runtime, _>([opaque].iter())
+#[test_case([submit_call()].into() => submit_call_expected(); "Submit data 0")]
+#[test_case([submit_bridge_call()].into() => submit_bridge_call_expected(); "Test bridge tx")]
+#[test_case([submit_call(), submit_bridge_call()].into() => submit_bridge_and_data_call_expected(); "Test blob and bridge tx")]
+fn data_root_filter(extrinsics: Vec<Vec<u8>>) -> H256 {
+	// println!("{:?}", extrinsic);
+	let mut opaque = vec![];
+	for extrinsic in extrinsics {
+		let o = OpaqueExtrinsic::decode(&mut extrinsic.as_slice()).unwrap();
+		opaque.push(o)
+	}
+	extrinsics_root::<Runtime, _>(opaque.iter())
 }
