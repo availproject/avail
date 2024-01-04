7413cb7e29ca455afdc5629ba50bd8ec9de23ac5
Selman Tanis <56926590+sSelmann@users.noreply.github.com>
2024-01-04 11:14:01 +0300

    Add config to deno example benchmarks (#355)
    * Add config to deno example benchmarks
    
    * Add config to deno examples
    
    * Add config only to deno example benchmarks

---
+++ examples/deno/benchmarks/config.ts
@@ -0,0 +1,6 @@
+export default {
+    seed: "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice",
+    endpoint: "ws://127.0.0.1:9944",
+    txCount: 100,
+    jobCount: 10,
+}
