9d2059f6b7dbc6a6ef0b09a18092402254433eec
Sasa Prsic <sasa@ethernal.tech>
2024-01-08 14:57:16 +0100

    Fix blob root response.

--- e2e/src/main.rs
+++ e2e/src/main.rs
@@ -357,6 +357,6 @@ pub mod tests {
 
 		let expected_proof_root = merkle_proof::<Keccak256, _, _>(vec![example_data.to_vec()], 0);
 		let actual_proof = query_data_proof(rpc, 1, block_hash).await.unwrap();
-		assert_eq!(actual_proof.root, expected_proof_root.root);
+		assert_eq!(actual_proof.blob_root, expected_proof_root.root);
 	}
 }
