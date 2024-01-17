0a1b1a764f6da1772d3660bbd0ea23cc2c9ea973
Leouarz <ghalielouarzazi@hotmail.com>
2024-01-17 13:04:32 +0100

    update comment

--- core/src/header_version/mod.rs
+++ core/src/header_version/mod.rs
@@ -6,7 +6,7 @@ use sp_runtime_interface::pass_by::PassByCodec;
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Encode, Decode, TypeInfo)]
 #[cfg_attr(feature = "runtime", derive(PassByCodec))]
 pub enum HeaderVersion {
-	V1, // Current one
-	V2, // To be used after runtime upgrade (new data_root)
+	V1,
+	V2, // Current one
 	V3, // To be used after removing padding_tail_value
 }
