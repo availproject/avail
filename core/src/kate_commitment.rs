ff78ae2c25aaa0b1f5e13745c368322ae9442518
Leouarz <ghalielouarzazi@hotmail.com>
2024-02-22 12:06:55 +0100

    remove header v1 & v2, add v3

--- core/src/kate_commitment.rs
+++ core/src/kate_commitment.rs
@@ -10,41 +10,7 @@ use sp_core::hexdisplay::HexDisplay;
 #[cfg(feature = "serde")]
 use sp_std::fmt;
 
-pub mod v1 {
-	use super::*;
-
-	/// Customized extrinsics root to save the commitment.
-	#[derive(PartialEq, Eq, Clone, Default, Encode, Decode, TypeInfo)]
-	#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-	#[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
-	#[cfg_attr(feature = "serde", serde(deny_unknown_fields))]
-	pub struct KateCommitment {
-		/// Rows
-		#[codec(compact)]
-		pub rows: u16,
-		/// Cols
-		#[codec(compact)]
-		pub cols: u16,
-		/// Plonk commitment.
-		pub commitment: Vec<u8>,
-		/// The merkle root of the data submitted
-		pub data_root: H256,
-	}
-
-	#[cfg(feature = "serde")]
-	impl fmt::Debug for KateCommitment {
-		fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-			f.debug_struct("KateCommitment(v1)")
-				.field("rows", &self.rows)
-				.field("cols", &self.cols)
-				.field("data_root", &HexDisplay::from(&self.data_root.as_ref()))
-				.field("commitment", &HexDisplay::from(&self.commitment.as_slice()))
-				.finish()
-		}
-	}
-}
-
-pub mod v2 {
+pub mod v3 {
 	use super::*;
 
 	/// Customized extrinsics root to save the commitment.
@@ -82,7 +48,7 @@ pub mod v2 {
 			let commitment = self.commitment.as_slice();
 			let data_root = self.data_root.as_ref();
 
-			f.debug_struct("KateCommitment(v2)")
+			f.debug_struct("KateCommitment(v3)")
 				.field("rows", &self.rows)
 				.field("cols", &self.cols)
 				.field("commitment", &HexDisplay::from(&commitment))
