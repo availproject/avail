18e5e48468c9ded99cd86a60a2287e6d380ca861
Marko Petrlic <petrlic.marko@gmail.com>
2024-03-05 15:28:27 +0100

    follow up

--- runtime/src/weights/mod.rs
+++ runtime/src/weights/mod.rs
@@ -1,6 +1,5 @@
 pub mod frame_system;
 pub mod pallet_balances;
-pub mod pallet_bounties;
 pub mod pallet_dactr;
 pub mod pallet_identity;
 pub mod pallet_indices;
@@ -9,7 +8,6 @@ pub mod pallet_proxy;
 pub mod pallet_scheduler;
 pub mod pallet_staking;
 pub mod pallet_timestamp;
-pub mod pallet_tips;
 pub mod pallet_treasury;
 pub mod pallet_tx_pause;
 pub mod pallet_utility;
