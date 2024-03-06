d618754712324ec389be05fbfc87d70bda938f37
Toufeeq Pasha <toufeeq@sovereignwallet.network>
2024-03-06 18:16:34 +0530

    Persisting AllExtrinsicsLen for debug purpose

--- pallets/system/src/lib.rs
+++ pallets/system/src/lib.rs
@@ -1790,6 +1790,7 @@ impl<T: Config> Pallet<T> {
 		// Remove previous block data from storage
 		BlockWeight::<T>::kill();
 		FailedExtrinsicIndices::<T>::kill();
+		AllExtrinsicsLen::<T>::kill();
 	}
 
 	/// Remove temporary "environment" entries in storage, compute the storage root and return the
@@ -1831,7 +1832,8 @@ impl<T: Config> Pallet<T> {
 			).deconstruct(),
 		);
 		ExecutionPhase::<T>::kill();
-		AllExtrinsicsLen::<T>::kill();
+		// This will be cleared in on_initialise of next block
+		// AllExtrinsicsLen::<T>::kill();
 
 		storage::unhashed::kill(well_known_keys::INTRABLOCK_ENTROPY);
 		// The following fields
