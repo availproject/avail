f9da447819f29f3d37427ea383c57e245618f482
Toufeeq Pasha <toufeeqpasha.2014@gmail.com>
2024-03-18 19:03:00 +0530

    fixed create_app_key example

--- avail-subxt/examples/create_app_key.rs
+++ avail-subxt/examples/create_app_key.rs
@@ -1,30 +1,29 @@
 use anyhow::Result;
 use avail_subxt::{
-	api, api::runtime_types::bounded_collections::bounded_vec::BoundedVec, avail, build_client,
-	tx_send_in_block, AvailConfig, Opts,
+	api, api::runtime_types::bounded_collections::bounded_vec::BoundedVec, tx, AvailClient, Opts,
 };
 use structopt::StructOpt;
-use subxt::{ext::sp_core::Pair, tx::PairSigner};
+use subxt_signer::sr25519::dev;
 
 /// This example demonstrates creation of application key.
-const ALICE_SEED: &str =
-	"bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice";
 
 #[async_std::main]
 async fn main() -> Result<()> {
 	let args = Opts::from_args();
-	let (client, _) = build_client(args.ws, args.validate_codegen).await?;
+	let client = AvailClient::new(args.ws).await?;
 
 	// Account
-	let alice = avail::Pair::from_string_with_seed(ALICE_SEED, None).unwrap();
-	let signer = PairSigner::<AvailConfig, avail::Pair>::new(alice.0);
+	let alice = dev::alice();
+	let alice_id = alice.public_key().into();
+	let nonce = client.tx().account_nonce(&alice_id).await?;
 
 	let call = api::tx()
 		.data_availability()
 		.create_application_key(BoundedVec(b"my_app_name".to_vec()));
+	let progress = tx::send_with_nonce(&client, &call, &alice, 0, nonce).await?;
 
-	let block = tx_send_in_block!(&client, &call, &signer).block_hash();
+	let hash = tx::then_in_block(progress).await?.block_hash();
 
-	println!("Application key created, block hash: {block:?}");
+	println!("Application key created, block hash: {hash:?}");
 	Ok(())
 }
