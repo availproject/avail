834af5af7fc7c5275751b7534a888af0637b8cfd
Marko Petrlic <petrlic.marko@gmail.com>
2024-03-27 00:13:46 +0100

    Added more targets to releaser ci

--- scripts/binaries/build.sh
+++ scripts/binaries/build.sh
@@ -6,6 +6,7 @@ ARCH="${ARCH:-x86_64}"
 
 IMAGE="${DISTRO}.Dockerfile"
 DOCKER_FILE="./scripts/binaries/$ARCH/$IMAGE"
+DOCKER_IGNORE_FILE="./scripts/binaries/$ARCH/shared.dockerignore"
 
 if ! test -f "$DOCKER_FILE"; then
     echo "Unknown option"
@@ -21,7 +22,7 @@ echo "Selected arch: $ARCH"
 echo "Selected docker file: $DOCKER_FILE"
 
 # Build the image
-"$ENGINE" build -t availnodet -f $DOCKER_FILE .
+"$ENGINE" build -t availnodet --ignorefile=$DOCKER_IGNORE_FILE -f $DOCKER_FILE .
 
 mkdir -p "output/$ARCH/$DISTRO"
 
