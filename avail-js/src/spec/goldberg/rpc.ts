5e2f489a196c2e25258ce632215d8ed260c37a1e
Leouarz <ghalielouarzazi@hotmail.com>
2024-04-02 15:50:47 +0200

    export goldberg types for backward compatibility

---
+++ avail-js/src/spec/goldberg/rpc.ts
@@ -0,0 +1,76 @@
+export const goldbergRpc = {
+    kate: {
+      blockLength: {
+        description: "Get Block Length",
+        params: [
+          {
+            name: "at",
+            type: "Hash",
+            isOptional: true,
+          },
+        ],
+        type: "BlockLength",
+      },
+      queryProof: {
+        description: "Generate the kate proof for the given `cells`",
+        params: [
+          {
+            name: "cells",
+            type: "Vec<Cell>",
+          },
+          {
+            name: "at",
+            type: "Hash",
+            isOptional: true,
+          },
+        ],
+        type: "Vec<u8>",
+      },
+      queryDataProof: {
+        description: "Generate the data proof for the given `index`",
+        params: [
+          {
+            name: "data_index",
+            type: "u32",
+          },
+          {
+            name: "at",
+            type: "Hash",
+            isOptional: true,
+          },
+        ],
+        type: "DataProof",
+      },
+      queryAppData: {
+        description: "Query app data with a specific app id",
+        params: [
+          {
+            name: "app_id",
+            type: "AppId",
+          },
+          {
+            name: "at",
+            type: "Hash",
+            isOptional: true,
+          },
+        ],
+        type: "Vec<Vec<u8>>",
+      },
+      queryRows: {
+        description: "Query rows based on their indices",
+        params: [
+          {
+            name: "rows",
+            type: "Vec<u32>",
+          },
+          {
+            name: "at",
+            type: "Hash",
+            isOptional: true,
+          },
+        ],
+        type: "Vec<Vec<u8>>",
+      },
+    },
+  }
+  
