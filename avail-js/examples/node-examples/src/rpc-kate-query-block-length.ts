29f73c8f0396330020250158243f04b094cd3de6
Leouarz <ghalielouarzazi@hotmail.com>
2024-04-10 15:30:05 +0200

    remove rpc from other places, publish new avail-js with format

--- avail-js/examples/node-examples/src/rpc-kate-query-block-length.ts
+++ avail-js/examples/node-examples/src/rpc-kate-query-block-length.ts
@@ -1,5 +1,5 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
-/// The example showcases how to programmatically call query block length RPC. 
+/// The example showcases how to programmatically call query block length RPC.
 ///
 
 import { initialize, getKeyringFromSeed } from "avail-js-sdk"
@@ -10,7 +10,7 @@ import config from "../../config"
 
 const main = async () => {
   try {
-    const api = await initialize(config.endpoint);
+    const api = await initialize(config.endpoint)
     const account = getKeyringFromSeed(config.seed)
     const appId = config.appId === 0 ? 1 : config.appId
     const options = { app_id: appId, nonce: -1 }
@@ -18,41 +18,40 @@ const main = async () => {
     // submit data transaction
     const txResult = await new Promise<ISubmittableResult>((res) => {
       api.tx.dataAvailability.submitData("Hello World").signAndSend(account, options, (result: ISubmittableResult) => {
-        console.log(`Tx status: ${result.status}`);
-          if (result.isFinalized || result.isError) {
-            res(result);
-          }
-        },
-      );
-    });
+        console.log(`Tx status: ${result.status}`)
+        if (result.isFinalized || result.isError) {
+          res(result)
+        }
+      })
+    })
 
     // Rejected Transaction handling
     if (txResult.isError) {
-      console.log(`Transaction was not executed`);
-      process.exit(1);
-    } 
-
-    const [txHash, blockHash] = [txResult.txHash as H256, txResult.status.asFinalized as H256];
-    console.log(`Tx Hash: ${txHash}, Block Hash: ${blockHash}`);
-
-    // Failed Transaction handling
-    const error = txResult.dispatchError;
-    if (error != undefined) {
-      if (error.isModule) {
-        const decoded = api.registry.findMetaError(error.asModule);
-        const { docs, name, section } = decoded;
-        console.log(`${section}.${name}: ${docs.join(' ')}`);
-      } else {
-        console.log(error.toString());
-      }
-      process.exit(1);
+      console.log(`Transaction was not executed`)
+      process.exit(1)
     }
 
-    const rpc: any = api.rpc;
-    const blockLength = await rpc.kate.blockLength(blockHash);
-    console.log(blockLength.toHuman());
+    const [txHash, blockHash] = [txResult.txHash as H256, txResult.status.asFinalized as H256]
+    console.log(`Tx Hash: ${txHash}, Block Hash: ${blockHash}`)
 
-    process.exit(0);
+    // Failed Transaction handling
+    const error = txResult.dispatchError
+    if (error != undefined) {
+      if (error.isModule) {
+        const decoded = api.registry.findMetaError(error.asModule)
+        const { docs, name, section } = decoded
+        console.log(`${section}.${name}: ${docs.join(" ")}`)
+      } else {
+        console.log(error.toString())
+      }
+      process.exit(1)
+    }
+
+    const rpc: any = api.rpc
+    const blockLength = await rpc.kate.blockLength(blockHash)
+    console.log(blockLength.toHuman())
+
+    process.exit(0)
   } catch (err) {
     console.error(err)
     process.exit(1)
