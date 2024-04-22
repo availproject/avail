8f3acac212673e187c415bc9a46302cbcb353aed
Leouarz <ghalielouarzazi@hotmail.com>
2024-04-22 19:32:16 +0200

    fix vec import again

--- core/src/data_lookup/mod.rs
+++ core/src/data_lookup/mod.rs
@@ -2,6 +2,7 @@ use codec::{Decode, Encode, Input};
 use core::convert::TryFrom;
 use scale_info::{Type, TypeInfo};
 use sp_core::RuntimeDebug;
+use sp_std::vec;
 use sp_std::{ops::Range, vec::Vec};
 use thiserror_no_std::Error;
 
