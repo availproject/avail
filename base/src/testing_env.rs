9bcf410fdebd0d5a49ec1f9b01bd428c8785bebe
Marko Petrlic <petrlic.marko@gmail.com>
2024-05-31 12:11:06 +0200

    Added RPC for custom grid data and testing environment

---
+++ base/src/testing_env.rs
@@ -0,0 +1,7 @@
+use avail_core::app_extrinsic::AppExtrinsic;
+use sp_std::vec::Vec;
+
+pub static mut ENABLE_TEST_EXTENSION_FAILURE: bool = false;
+pub static mut ENABLE_TEST_GRID_FAILURE: bool = false;
+pub static mut ENABLE_TEST_COMMITMENT_FAILURE: bool = false;
+pub static mut TEST_POPULATE_GRID: Option<Vec<AppExtrinsic>> = None;
---
+++ rpc/testing-rpc/src/lib.rs
@@ -0,0 +1,54 @@
+use avail_base::testing_env::*;
+use avail_core::app_extrinsic::AppExtrinsic;
+use jsonrpsee::{
+	core::{async_trait, RpcResult},
+	proc_macros::rpc,
+};
+use std::marker::Sync;
+
+/// # TODO
+#[rpc(client, server)]
+pub trait TestingApi {
+	#[method(name = "testing_toggleExtensionFailure")]
+	async fn toggle_extension_failure(&self, value: bool) -> RpcResult<()>;
+	#[method(name = "testing_toggleGridFailure")]
+	async fn toggle_grid_failure(&self, value: bool) -> RpcResult<()>;
+	#[method(name = "testing_toggleCommitmentFailure")]
+	async fn toggle_commitment_failure(&self, value: bool) -> RpcResult<()>;
+	#[method(name = "testing_populateGrid")]
+	async fn populate_grid(&self, value: Option<Vec<AppExtrinsic>>) -> RpcResult<()>;
+}
+
+pub struct TestingEnv;
+
+#[async_trait]
+impl TestingApiServer for TestingEnv {
+	async fn toggle_extension_failure(&self, value: bool) -> RpcResult<()> {
+		unsafe {
+			ENABLE_TEST_EXTENSION_FAILURE = value;
+		}
+
+		Ok(())
+	}
+	async fn toggle_grid_failure(&self, value: bool) -> RpcResult<()> {
+		unsafe {
+			ENABLE_TEST_GRID_FAILURE = value;
+		}
+
+		Ok(())
+	}
+	async fn toggle_commitment_failure(&self, value: bool) -> RpcResult<()> {
+		unsafe {
+			ENABLE_TEST_COMMITMENT_FAILURE = value;
+		}
+
+		Ok(())
+	}
+	async fn populate_grid(&self, value: Option<Vec<AppExtrinsic>>) -> RpcResult<()> {
+		unsafe {
+			TEST_POPULATE_GRID = value;
+		}
+
+		Ok(())
+	}
+}
