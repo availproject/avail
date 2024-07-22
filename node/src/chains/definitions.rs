056d9d12b21aab9a8818b3976f09e5ac8aa54d45
Toufeeq Pasha <toufeeq@sovereignwallet.network>
2024-07-22 16:22:02 +0530

    added ss58Format property

--- node/src/chains/definitions.rs
+++ node/src/chains/definitions.rs
@@ -34,7 +34,7 @@ pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Pu
 
 /// Common properties for chains
 pub fn chain_properties() -> Properties {
-	serde_json::json!({ "tokenDecimals": 18, "tokenSymbol": "AVAIL" })
+	serde_json::json!({ "ss58Format": 42 , "tokenDecimals": 18, "tokenSymbol": "AVAIL"})
 		.as_object()
 		.expect("Map given; qed")
 		.clone()
