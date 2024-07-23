bcf5efafaa086a44b69c0bf19601c203678f6345
Leouarz <ghalielouarzazi@hotmail.com>
2024-07-23 07:02:46 +0200

    update avail-js and add endpoint

--- avail-js/examples/node-examples/src/tx-staking-payout-stakers.ts
+++ avail-js/examples/node-examples/src/tx-staking-payout-stakers.ts
@@ -20,7 +20,6 @@ const main = async () => {
   // Number of days to check (how many era we'll check for pending rewards)
   let nbEra = 1
 
-
   /* THINGS YOU SHOULD NOT CHANGE */
   // Get the active era
   const activeEra = (await api.query.staking.currentEra()).toJSON() as number
@@ -30,41 +29,50 @@ const main = async () => {
   const startEra = activeEra > nbEra ? activeEra - nbEra : 0
 
   // We set a list of eras and validators to claim for
-  let toClaim: { era: number, validator: string }[] = []
+  let toClaim: { era: number; validator: string }[] = []
 
   for (let i = startEra; i < activeEra; i++) {
     // We get the validators who earned reward during this era
-    const eraRewardPoints = (await api.query.staking.erasRewardPoints(i)).toJSON() as { total: number, individual: { [address: string]: number } }
+    const eraRewardPoints = (await api.query.staking.erasRewardPoints(i)).toJSON() as {
+      total: number
+      individual: { [address: string]: number }
+    }
     const eraRewardPointsValidatorList = Object.keys(eraRewardPoints.individual)
 
     // We get the validators where the payout has already been done for this era
-    const claimedRewards = (await api.query.staking.claimedRewards.entries(i)).map(x => (x[0].toHuman() as string[])[1])
+    const claimedRewards = (await api.query.staking.claimedRewards.entries(i)).map(
+      (x) => (x[0].toHuman() as string[])[1],
+    )
 
     // We get all validator WITH eraRewardPoints and WITHOUT already claimed reward
-    let validatorsWithPendingClaim = eraRewardPointsValidatorList.filter(x => !claimedRewards.includes(x))
+    let validatorsWithPendingClaim = eraRewardPointsValidatorList.filter((x) => !claimedRewards.includes(x))
 
     // We filter by the specified stashes if there are any
     if (validatorStashes.length > 0) {
-      validatorsWithPendingClaim = validatorsWithPendingClaim.filter(x => validatorStashes.includes(x))
+      validatorsWithPendingClaim = validatorsWithPendingClaim.filter((x) => validatorStashes.includes(x))
     }
 
     // We update the global list
-    toClaim = [...toClaim, ...validatorsWithPendingClaim.map(x => { return { era: i, validator: x } })]
+    toClaim = [
+      ...toClaim,
+      ...validatorsWithPendingClaim.map((x) => {
+        return { era: i, validator: x }
+      }),
+    ]
     console.log(`Found ${validatorsWithPendingClaim.length} validators with pending claims for era ${i}`)
   }
 
   // We create all the transactions
-  const transactions = await Promise.all(toClaim.map(x => api.tx.staking.payoutStakers(x.validator, x.era)))
+  const transactions = await Promise.all(toClaim.map((x) => api.tx.staking.payoutStakers(x.validator, x.era)))
   const chunks = []
   const chunkSize = 5
   for (let i = 0; i < transactions.length; i += chunkSize) {
-    const chunk = transactions.slice(i, i + chunkSize);
-    chunks.push(chunk);
+    const chunk = transactions.slice(i, i + chunkSize)
+    chunks.push(chunk)
   }
 
-
   // We batch them together
-  const batches = chunks.map(x => api.tx.utility.batchAll(x))
+  const batches = chunks.map((x) => api.tx.utility.batchAll(x))
   for (const [i, tx] of batches.entries()) {
     console.log(`Sending batch transaction ${i + 1} of ${batches.length}`)
 
