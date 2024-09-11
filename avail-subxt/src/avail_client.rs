901ffbf7fb855876ec7070295ab2363af6c90922
Toufeeq Pasha <toufeeqpasha.2014@gmail.com>
2024-09-11 03:47:57 +0200

    BREAKING: removed rpc_methods from AvailClient

--- avail-subxt/src/avail_client.rs
+++ avail-subxt/src/avail_client.rs
@@ -1,7 +1,6 @@
 use crate::AvailConfig;
 
 use core::ops::Deref;
-pub use jsonrpsee::core::client::Client as RpcMethods;
 use subxt::{
 	backend::{legacy::LegacyRpcMethods, rpc::RpcClient},
 	utils::validate_url_is_secure,
@@ -10,7 +9,6 @@ use subxt::{
 
 #[derive(Debug)]
 pub struct AvailClient {
-	rpc_methods: RpcMethods,
 	online: OnlineClient<AvailConfig>,
 	rpc: RpcClient,
 }
@@ -18,33 +16,17 @@ pub struct AvailClient {
 impl AvailClient {
 	pub async fn new<U: AsRef<str>>(ws_uri: U) -> Result<Self, Error> {
 		validate_url_is_secure(ws_uri.as_ref())?;
-		let rpc_methods = jsonrpsee_helpers::client(ws_uri.as_ref())
-			.await
-			.map_err(|e| Error::Other(format!("Client cannot be created: {e:?}")))?;
-
 		let rpc = RpcClient::from_url(ws_uri).await?;
 		let online = OnlineClient::<AvailConfig>::from_rpc_client(rpc.clone()).await?;
 
-		Ok(AvailClient {
-			rpc_methods,
-			online,
-			rpc,
-		})
+		Ok(AvailClient { online, rpc })
 	}
 
 	pub async fn new_insecure<U: AsRef<str>>(ws_uri: U) -> Result<Self, Error> {
-		let rpc_methods = jsonrpsee_helpers::client(ws_uri.as_ref())
-			.await
-			.map_err(|e| Error::Other(format!("Client cannot be created: {e:?}")))?;
-
 		let rpc = RpcClient::from_insecure_url(ws_uri).await?;
 		let online = OnlineClient::<AvailConfig>::from_rpc_client(rpc.clone()).await?;
 
-		Ok(AvailClient {
-			rpc_methods,
-			online,
-			rpc,
-		})
+		Ok(AvailClient { online, rpc })
 	}
 
 	pub fn legacy_rpc(&self) -> LegacyRpcMethods<AvailConfig> {
@@ -58,43 +40,11 @@ impl AvailClient {
 	pub fn online(&self) -> &OnlineClient<AvailConfig> {
 		&self.online
 	}
-
-	pub fn rpc_methods(&self) -> &RpcMethods {
-		&self.rpc_methods
-	}
 }
 
 impl Deref for AvailClient {
 	type Target = OnlineClient<AvailConfig>;
 	fn deref(&self) -> &Self::Target {
 		&self.online
-	}
-}
-
-// #[cfg(feature = "native")]
-mod jsonrpsee_helpers {
-	pub use jsonrpsee::{
-		client_transport::ws::{self, EitherStream, Url, WsTransportClientBuilder},
-		core::client::{Client, Error},
-	};
-	use tokio_util::compat::Compat;
-
-	pub type Sender = ws::Sender<Compat<EitherStream>>;
-	pub type Receiver = ws::Receiver<Compat<EitherStream>>;
-
-	/// Build WS RPC client from URL
-	pub async fn client(url: &str) -> Result<Client, Error> {
-		let (sender, receiver) = ws_transport(url).await?;
-		Ok(Client::builder()
-			.max_buffer_capacity_per_subscription(4096)
-			.build_with_tokio(sender, receiver))
-	}
-
-	async fn ws_transport(url: &str) -> Result<(Sender, Receiver), Error> {
-		let url = Url::parse(url).map_err(|e| Error::Transport(e.into()))?;
-		WsTransportClientBuilder::default()
-			.build(url)
-			.await
-			.map_err(|e| Error::Transport(e.into()))
 	}
 }
