84d10e553e7f4aa487dcddfd06190aeac896ccd8
Leouarz <ghalielouarzazi@hotmail.com>
2024-11-29 23:19:18 +0100

    Better fusion (#691)
    * fusion without staking pallet
    
    * update deps

--- avail-rust/src/api_dev.rs
+++ avail-rust/src/api_dev.rs
@@ -32117,7 +32117,7 @@ pub mod api {
 				#[codec(index = 41)]
 				TxPause(runtime_types::pallet_tx_pause::pallet::Call),
 				#[codec(index = 42)]
-				TreasuryCommittee(runtime_types::pallet_collective::pallet::Call),
+				TreasuryCommittee(runtime_types::pallet_collective::pallet::Call2),
 				#[codec(index = 43)]
 				Fusion(runtime_types::pallet_fusion::pallet::Call),
 			}
@@ -33991,6 +33991,79 @@ pub mod api {
 				)]
 				#[doc = "Contains a variant per dispatchable extrinsic that this pallet has."]
 				pub enum Call {
+					#[codec(index = 0)]
+					#[doc = "See [`Pallet::set_members`]."]
+					set_members {
+						new_members: ::subxt::ext::subxt_core::alloc::vec::Vec<
+							::subxt::ext::subxt_core::utils::AccountId32,
+						>,
+						prime: ::core::option::Option<::subxt::ext::subxt_core::utils::AccountId32>,
+						old_count: ::core::primitive::u32,
+					},
+					#[codec(index = 1)]
+					#[doc = "See [`Pallet::execute`]."]
+					execute {
+						proposal: ::subxt::ext::subxt_core::alloc::boxed::Box<
+							runtime_types::da_runtime::RuntimeCall,
+						>,
+						#[codec(compact)]
+						length_bound: ::core::primitive::u32,
+					},
+					#[codec(index = 2)]
+					#[doc = "See [`Pallet::propose`]."]
+					propose {
+						#[codec(compact)]
+						threshold: ::core::primitive::u32,
+						proposal: ::subxt::ext::subxt_core::alloc::boxed::Box<
+							runtime_types::da_runtime::RuntimeCall,
+						>,
+						#[codec(compact)]
+						length_bound: ::core::primitive::u32,
+					},
+					#[codec(index = 3)]
+					#[doc = "See [`Pallet::vote`]."]
+					vote {
+						proposal: ::subxt::ext::subxt_core::utils::H256,
+						#[codec(compact)]
+						index: ::core::primitive::u32,
+						approve: ::core::primitive::bool,
+					},
+					#[codec(index = 5)]
+					#[doc = "See [`Pallet::disapprove_proposal`]."]
+					disapprove_proposal {
+						proposal_hash: ::subxt::ext::subxt_core::utils::H256,
+					},
+					#[codec(index = 6)]
+					#[doc = "See [`Pallet::close`]."]
+					close {
+						proposal_hash: ::subxt::ext::subxt_core::utils::H256,
+						#[codec(compact)]
+						index: ::core::primitive::u32,
+						proposal_weight_bound: runtime_types::sp_weights::weight_v2::Weight,
+						#[codec(compact)]
+						length_bound: ::core::primitive::u32,
+					},
+				}
+				#[derive(
+					:: subxt :: ext :: subxt_core :: ext :: codec :: Decode,
+					:: subxt :: ext :: subxt_core :: ext :: codec :: Encode,
+					:: subxt :: ext :: subxt_core :: ext :: scale_decode :: DecodeAsType,
+					:: subxt :: ext :: subxt_core :: ext :: scale_encode :: EncodeAsType,
+					Clone,
+					Debug,
+					Eq,
+					PartialEq,
+				)]
+				# [codec (crate = :: subxt :: ext :: subxt_core :: ext :: codec)]
+				#[codec(dumb_trait_bound)]
+				#[decode_as_type(
+					crate_path = ":: subxt :: ext :: subxt_core :: ext :: scale_decode"
+				)]
+				#[encode_as_type(
+					crate_path = ":: subxt :: ext :: subxt_core :: ext :: scale_encode"
+				)]
+				#[doc = "Contains a variant per dispatchable extrinsic that this pallet has."]
+				pub enum Call2 {
 					#[codec(index = 0)]
 					#[doc = "See [`Pallet::set_members`]."]
 					set_members {
