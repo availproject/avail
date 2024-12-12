a9a019aa38257927b95f25b35e43cd476372cc67
Toufeeq Pasha <toufeeqpasha.2014@gmail.com>
2024-12-12 21:42:38 +0530

    fmt

--- runtime/src/primitives.rs
+++ runtime/src/primitives.rs
@@ -75,7 +75,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	Migrations
+	Migrations,
 >;
 
 /// ID type for named reserves.
