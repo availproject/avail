72c2a373ea9d55ad6948402fa769943a1e39cb25
taozui472 <taozui472@gmail.com>
2025-02-14 11:30:50 +0800

    Updated copyright year to 2025

--- avail-rust/src/primitives/block/extrinsics_params.rs
+++ avail-rust/src/primitives/block/extrinsics_params.rs
@@ -1,4 +1,4 @@
-// Copyright 2019-2024 Parity Technologies (UK) Ltd.
+// Copyright 2019-2025 Parity Technologies (UK) Ltd.
 // This file is dual-licensed as Apache-2.0 or GPL-3.0.
 // see LICENSE for license details.
 
