7dd8662a0aa1a3785210a690530b1f1581a5fb12
Marko Petrlic <petrlic.marko@gmail.com>
2025-03-14 23:55:27 +0100

    Improved transaction filtering

--- pallets/dactr/src/extensions/extensions_mock.rs
+++ pallets/dactr/src/extensions/extensions_mock.rs
@@ -1,12 +1,22 @@
 #![cfg(test)]
 
+use codec::{Decode, Encode, MaxEncodedLen};
+use frame_support::pallet_prelude::RuntimeDebug;
+use frame_support::parameter_types;
+use frame_support::traits::fungible::HoldConsideration;
+use frame_support::traits::EqualPrivilegeOnly;
+use frame_support::traits::InstanceFilter;
+use frame_support::traits::LinearStoragePrice;
 use frame_support::weights::WeightToFee;
 use frame_support::{derive_impl, weights::IdentityFee};
+use frame_system::EnsureRoot;
 use frame_system::{
 	mocking::MockUncheckedExtrinsic, native::hosted_header_builder::da::HeaderExtensionBuilder,
 	test_utils::TestRandomness,
 };
 use pallet_transaction_payment::FungibleAdapter;
+use sp_core::ConstU32;
+use sp_runtime::traits::BlakeTwo256;
 use sp_runtime::{AccountId32, BuildStorage};
 
 use crate::{self as da_control, *};
@@ -26,6 +36,10 @@ frame_support::construct_runtime!(
 		TransactionPayment: pallet_transaction_payment,
 		DataAvailability: da_control,
 		Vector: pallet_vector,
+		Multisig: pallet_multisig,
+		Proxy: pallet_proxy,
+		Scheduler: pallet_scheduler,
+		Preimage: pallet_preimage,
 	}
 );
 
@@ -81,6 +95,129 @@ impl pallet_vector::Config for Test {
 
 #[derive_impl(pallet_timestamp::config_preludes::TestDefaultConfig as pallet_timestamp::DefaultConfig)]
 impl pallet_timestamp::Config for Test {}
+
+parameter_types! {
+	// One storage item; key size 32, value size 8; .
+	pub const ProxyDepositBase: Balance = 10 * 1;
+	// Additional storage item size of 33 bytes.
+	pub const ProxyDepositFactor: Balance = 3 * 1;
+	pub const AnnouncementDepositBase: Balance = 10 * 1;
+	pub const AnnouncementDepositFactor: Balance = 5 * 1;
+}
+
+/// The type used to represent the kinds of proxying allowed.
+#[derive(
+	Copy,
+	Clone,
+	Eq,
+	PartialEq,
+	Ord,
+	PartialOrd,
+	Encode,
+	Decode,
+	RuntimeDebug,
+	MaxEncodedLen,
+	scale_info::TypeInfo,
+)]
+pub enum ProxyType {
+	Any,
+	NonTransfer,
+	Governance,
+	Staking,
+	IdentityJudgement,
+	NominationPools,
+}
+impl Default for ProxyType {
+	fn default() -> Self {
+		Self::Any
+	}
+}
+impl InstanceFilter<RuntimeCall> for ProxyType {
+	fn filter(&self, c: &RuntimeCall) -> bool {
+		match self {
+			_ => true,
+		}
+	}
+	fn is_superset(&self, o: &Self) -> bool {
+		match (self, o) {
+			(x, y) if x == y => true,
+			(ProxyType::Any, _) => true,
+			(_, ProxyType::Any) => false,
+			(ProxyType::NonTransfer, _) => true,
+			_ => false,
+		}
+	}
+}
+
+impl pallet_proxy::Config for Test {
+	type RuntimeCall = RuntimeCall;
+	type RuntimeEvent = RuntimeEvent;
+	type Currency = Balances;
+	type ProxyType = ProxyType;
+	type ProxyDepositBase = ProxyDepositBase;
+	type ProxyDepositFactor = ProxyDepositFactor;
+	type MaxProxies = ConstU32<32>;
+	type WeightInfo = ();
+	type MaxPending = ConstU32<32>;
+	type CallHasher = BlakeTwo256;
+	type AnnouncementDepositBase = AnnouncementDepositBase;
+	type AnnouncementDepositFactor = AnnouncementDepositFactor;
+}
+
+parameter_types! {
+	pub MaximumSchedulerWeight: Weight = (Perbill::from_percent(80) * 1).into();
+	pub const MaxScheduledPerBlock: u32 = 50;
+}
+
+impl pallet_scheduler::Config for Test {
+	type RuntimeCall = RuntimeCall;
+	type RuntimeEvent = RuntimeEvent;
+	type MaxScheduledPerBlock = ConstU32<512>;
+	type MaximumWeight = MaximumSchedulerWeight;
+	type OriginPrivilegeCmp = EqualPrivilegeOnly;
+	type PalletsOrigin = OriginCaller;
+	type Preimages = Preimage;
+	type RuntimeOrigin = RuntimeOrigin;
+	type ScheduleOrigin = EnsureRoot<AccountId32>;
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const PreimageBaseDeposit: Balance = 1 * 1;
+	// One cent: $10,000 / MB
+	pub const PreimageByteDeposit: Balance = 1 * 1;
+	pub const PreimageHoldReason: RuntimeHoldReason = RuntimeHoldReason::Preimage(pallet_preimage::HoldReason::Preimage);
+}
+
+impl pallet_preimage::Config for Test {
+	type Consideration = HoldConsideration<
+		AccountId32,
+		Balances,
+		PreimageHoldReason,
+		LinearStoragePrice<PreimageBaseDeposit, PreimageByteDeposit, Balance>,
+	>;
+	type Currency = Balances;
+	type ManagerOrigin = EnsureRoot<AccountId32>;
+	type RuntimeEvent = RuntimeEvent;
+	type WeightInfo = ();
+}
+
+parameter_types! {
+	pub const DepositBase: Balance = 2 * 1;
+	// Additional storage item size of 32 bytes.
+	pub const DepositFactor: Balance = 5 * 1;
+}
+
+impl pallet_multisig::Config for Test {
+	type RuntimeEvent = RuntimeEvent;
+	type RuntimeCall = RuntimeCall;
+	type Currency = Balances;
+	type DepositBase = DepositBase;
+	type DepositFactor = DepositFactor;
+	type MaxSignatories = ConstU32<100>;
+	#[doc = " Weight information for extrinsics in this pallet."]
+	type WeightInfo = ();
+}
 
 fn u8_to_account_id(value: u8) -> AccountId32 {
 	let mut account = [0u8; 32];
