6584132f1ab5f19fa7537eaa06757bcb338e9abb
Marko Petrlic <petrlic.marko@gmail.com>
2025-10-03 02:31:17 +0200

    Updated script to run blob benchmarks

--- scripts/run_blob_benchmark.sh
+++ scripts/run_blob_benchmark.sh
@@ -1,19 +1,55 @@
 #!/bin/bash
 
-echo "--"
-echo "[HELP] You can run this script in two ways"
-echo "NORMAL: "
-echo "      ./scripts/run_blob_benchmark.sh PATH_TO_FILE"
-echo "      FILE=PATH_TO_FILE ./scripts/run_blob_benchmark.sh"
-echo "DOCKER:"
-echo "      ENGINE=podman ./scripts/run_blob_benchmark.sh PATH_TO_FILE"
-echo "      ENGINE=podman FILE=PATH_TO_FILE ./scripts/run_blob_benchmark.sh"
-echo ""
-echo "! Important: You must call this script from the root directory."
-echo "! Important: Working directory is ./blob so either use absolute paths or paths relative to ./blob directory."
-echo "--"
+# echo "--"
+# echo "[HELP] You can run this script in two ways"
+# echo "NORMAL: (will execute cargo command)"
+# echo "      ./scripts/run_blob_benchmark.sh PATH_TO_FILE"
+# echo "      FILE=PATH_TO_FILE ./scripts/run_blob_benchmark.sh"
+# echo "CONTAINER: (builds or runs existing containers)"
+# echo "      1. ./scripts/run_blob_benchmark.sh podman/docker build"
+# echo "      2. ./scripts/run_blob_benchmark.sh podman/docker run PATH_TO_FILE"
+# echo ""
+# echo "! Important: You must call this script from the root directory."
+# echo "! Important: In NORMAL mode: Working directory is ./blob so either use absolute paths or paths relative to ./blob directory."
+# echo "! Important: In DOCKER mode: Working directory is the root directory."
+# echo ""
+# echo "Example for a file name 32MiB in root directory:"
+# echo "  NORMAL:"
+# echo "      ./scripts/run_blob_benchmark.sh ./../32MiB"
+# echo ""
+# echo "  DOCKER:"
+# echo "      ./scripts/run_blob_benchmark.sh podman build # This is not necessary if you already run it once"
+# echo "      ./scripts/run_blob_benchmark.sh podman run ./32MiB"
+# echo "--"
 
+export CXXFLAGS="$CXXFLAGS -include cstdint"
+
+# CONTAINER
 FIRST_ARG="$1"
+if [[ "$FIRST_ARG" == "docker" || "$FIRST_ARG" == "podman" ]]; then
+    SECOND_ARG="$2"
+    if [[ "$SECOND_ARG" == "run" ]]; then
+        THIRD_ARG="$3"
+        if [[ -v THIRD_ARG ]]; then 
+            export FILE="$THIRD_ARG"
+        fi
+
+        if [[ ! -v FILE ]]; then 
+            echo "You need to set FILE= env variable to point to a file that you want to benchmark"
+            exit 1
+        fi
+
+        $FIRST_ARG run --rm -it -v ${PWD}:/work:z  myimg /work/"$FILE"
+    elif [[ "$SECOND_ARG" == "build" ]]; then
+        $FIRST_ARG build -f ./dockerfiles/blob-benchmark.Dockerfile --ignorefile ./dockerfiles/blob-benchmark.dockerignore .
+    else
+        echo "Supported comamnds: [build] [run]. Example: Docker build"
+    fi
+
+    exit 0
+fi
+
+# NORMAL
 if [[ -v FIRST_ARG ]]; then 
    export FILE="$FIRST_ARG"
 fi
@@ -23,15 +59,5 @@ if [[ ! -v FILE ]]; then
    exit 1
 fi
 
-if [[ ! -v ENGINE ]]; then 
-    cd blob
-    cargo bench -p avail-blob --bench submit_data
-fi
-
-if [[ "$ENGINE" != "docker" && "$ENGINE" != "podman" ]]; then
-    echo "ENGINE must be 'docker' or 'podman' (got: '$ENGINE')"
-    exit 1
-fi
-
-$ENGINE build -f ./dockerfiles/blob-benchmark.Dockerfile --ignorefile ./dockerfiles/blob-benchmark.dockerignore .
-$ENGINE run --rm -it myimg "$FILE"
+cd blob
+cargo bench -p avail-blob --bench submit_data
